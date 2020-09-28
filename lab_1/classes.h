2c037e6e4b73dafe1c5a9817d34096660ac17391
MOCTNK <71684726+MOCTNK@users.noreply.github.com>
2020-09-28 15:57:15 +0700

    Add files via upload

---
+++ lab_1/classes.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "functions.h";
+
+class Fraction {
+public:
+	Fraction();
+	Fraction(int get_numerator, int get_denominator);
+	static int count;
+	void set(int get_numerator, int get_denominator);
+	void add(Fraction b);
+	void subtract(Fraction b);
+	void multiply(Fraction b);
+	void divide(Fraction b);
+	int getNumerator() { return numerator; }
+	int getDenominator() { return denominator; }
+	char *getText() { return text; }
+private:
+	int numerator;
+	int denominator;
+	char* text;
+	bool error;
+	void reduction();
+};
---
+++ lab_1/lab_1.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include "classes.h";
+using namespace std;
+
+bool testAdd(Fraction &a, Fraction b, Fraction c) {
+	a.add(b);
+	if (a.getNumerator() == c.getNumerator() && a.getDenominator() == c.getDenominator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool testSubtract(Fraction &a, Fraction b, Fraction c) {
+	a.subtract(b);
+	if (a.getNumerator() == c.getNumerator() && a.getDenominator() == c.getDenominator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool testMultiply(Fraction &a, Fraction b, Fraction c) {
+	a.multiply(b);
+	if (a.getNumerator() == c.getNumerator() && a.getDenominator() == c.getDenominator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool testDivide(Fraction a, Fraction b, Fraction c) {
+	a.divide(b);
+	if (a.getNumerator() == c.getNumerator() && a.getDenominator() == c.getDenominator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main()
+{
+	Fraction a(3,5);
+	Fraction b(7, 11);
+	Fraction c(68, 55);
+	cout << a.getText() << " + " << b.getText();
+	if (testAdd(a, b, c) == true) {
+		cout << " = " << c.getText() << endl;
+		cout << "Fraction addition test completed successfully" << endl;
+	}
+	else {
+		cout << " != " << c.getText() << endl;
+		cout << "Fraction addition test completed failed" << endl;
+	}
+	cout << endl;
+
+	a.set(-5, 7);
+	b.set(20, 21);
+	c.set(-5, 3);
+	cout << a.getText() << " - " << b.getText();
+	if (testSubtract(a, b, c) == true) {
+		cout << " = " << c.getText() << endl;
+		cout << "Fraction subtraction test completed successfully" << endl;
+	}
+	else {
+		cout << " != " << c.getText();
+		cout << "Fraction subtraction test completed failed" << endl;
+	}
+	cout << endl;
+
+	a.set(2, 9);
+	b.set(27, 34);
+	c.set(3, 17);
+	cout << a.getText() << " * " << b.getText();
+	if (testMultiply(a, b, c) == true) {
+		cout << " = " << c.getText() << endl;
+		cout << "Fraction multiplication test completed successfully" << endl;
+	}
+	else {
+		cout << " != " << c.getText() << endl;
+		cout << "Fraction multiplication test completed failed" << endl;
+	}
+	cout << endl;
+
+	a.set(56, 75);
+	b.set(-45, 34);
+	c.set(-1904, 3375);
+	cout << a.getText() << " : " << b.getText();
+	if (testDivide(a, b, c) == true) {
+		cout << " = " << c.getText() << endl;
+		cout << "Fraction division test completed successfully" << endl;
+	}
+	else {
+		cout << " != " << c.getText() << endl;
+		cout << "Fraction division test completed failed" << endl;
+	}
+	cout << endl;
+
+	cout << Fraction::count << " fractions were initialized" <<endl;
+	return 0;
+}
