256b2ccce51ae71a46a4a43e0ffb972a9469a4c6
MOCTNK <71684726+MOCTNK@users.noreply.github.com>
2020-11-04 14:46:27 +0700

    Delete functions.h

--- lab_3/functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>;
-
-char* toString(int number);
-void increaseSize(char *&str);
-int random(const int min, const int max);
-void concatenation(char *&str1, char *str2);
