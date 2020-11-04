8797fedee2c5a4297a7d0314092ba4f4370a8915
MOCTNK <71684726+MOCTNK@users.noreply.github.com>
2020-11-04 14:47:55 +0700

    Add files via upload

---
+++ lab_3/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>;
+
+char* toString(int number);
+void increaseSize(char *&str);
+int random(const int min, const int max);
+void concatenation(char *&str1, char *str2);
+void disassemble(char *str, char *str1, char *str2);
